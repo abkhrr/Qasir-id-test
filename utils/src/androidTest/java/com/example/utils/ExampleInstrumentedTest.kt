b12f680c3165bd44ada10e8748fba02656d0067a
adyabukhari <bukhariadbuk@gmail.com>
2021-09-22 23:13:21 +0700

    Fixing All Dependencies

--- utils/src/androidTest/java/com/example/utils/ExampleInstrumentedTest.kt
+++ utils/src/androidTest/java/com/example/utils/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.abstraction
+package com.example.utils
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.abstraction.test", appContext.packageName)
+        assertEquals("com.example.utils.test", appContext.packageName)
     }
 }
--- utils/src/test/java/com/example/utils/ExampleUnitTest.kt
+++ utils/src/test/java/com/example/utils/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.abstraction
+package com.example.utils
 
 import org.junit.Test
 
